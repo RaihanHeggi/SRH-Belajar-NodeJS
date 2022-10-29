17bb476348446a8fa4d32b54a35953c5b17ee3e0
RaihanHeggi <syaraihanheggi@gmail.com>
2022-10-29 23:38:37 +0700

    Body Request and Routing Request

--- server.js
+++ server.js
@@ -1,28 +1,42 @@
 const http = require('http');
- 
+
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
     response.statusCode = 200;
- 
-    const { method } = request;
- 
-    if(method === 'GET') {
-        response.end('<h1>Hello!</h1>');
+
+    const {method, url} = request;
+
+    if(url ==='/'){
+        if(method === "GET"){
+            response.end('Ini adalah homepage');
+        }else{
+            response.end(`Halaman tidak dapat diakses dengan ${method} request`)
+        }
+    }else if (url === "/about"){
+        if(method === "GET"){
+            response.end('Halo! Ini adalah halaman about');
+        }else if(method === "POST"){
+            // persiapkan body request 
+            let body = []
+
+            request.on('data', (chunk) =>{
+                body.push(chunk);
+            });
+
+            request.on('end', () =>{
+                body = Buffer.concat(body).toString();
+                const {name} = JSON.parse(body);
+                response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
+            });
+
+        }else{
+            response.end(`Halaman tidak dapat diakses dengan ${method} request`)
+        }
+    }else{
+        response.end('<h1>Halaman tidak ditemukan!</h1>');
     }
- 
-    if(method === 'POST') {
-        response.end('<h1>Hai!</h1>');
-    }
- 
-    if(method === 'PUT') {
-        response.end('<h1>Bonjour!</h1>');
-    }
- 
-    if(method === 'DELETE') {
-        response.end('<h1>Salam!</h1>');
-    }
-};
- 
+}
+
 const server = http.createServer(requestListener);
  
 const port = 5000;
